8ec871fedee3ceca49e2eb19a35d3b821d206b93
techmaster <cuong@techmaster.vn>
2021-02-03 21:45:17 +0700

    Mid Term Project File upload

---
+++ midtermhackathon/03UploadMultipleFiles/demoupload/src/test/java/vn/techmaster/demoupload/DemouploadApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.techmaster.demoupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemouploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
