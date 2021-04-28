cf8e59776822dba8a5912301d91110cdce6f4d4b
thinhtran279 <thinhtv27988@gmail.com>
2021-04-28 18:18:44 +0700

    them noi dung vao slide

--- bean/06LazyPrototype/demobean/src/main/java/vn/techmaster/demobean/bean/Car.java
+++ bean/06LazyPrototype/demobean/src/main/java/vn/techmaster/demobean/bean/Car.java
@@ -1,5 +1,6 @@
 package vn.techmaster.demobean.bean;
 
+//import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Lazy;
@@ -26,8 +27,20 @@ public class Car {
     return "Car [engine=" + engine + ", steering=" + steering.steer() + "]";
   }
 
+ 
   //Trả về điểm bắt đầu và kết thúc trong hành trình
   public String navigate() {
     return streetMap.getRoute();
   }
+
+
+  /*
+  // Trong trường hợp không muốn dùng @Lazy, thì cách tiếp theo là sử dụng ObjectFactory. Khi getObject được gọi, spring sẽ khởi tạo 1 instance mới cho prototype bean
+  @Autowired
+  private ObjectFactory<StreetMap> streetMapFactory;
+
+  public String navigate() {
+    return streetMapFactory.getObject().getRoute();
+  }
+  */
 }
--- bean/06LazyPrototype/demobean/src/main/java/vn/techmaster/demobean/configuration/MapConfig.java
+++ bean/06LazyPrototype/demobean/src/main/java/vn/techmaster/demobean/configuration/MapConfig.java
@@ -1,17 +1,29 @@
 package vn.techmaster.demobean.configuration;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+//import org.springframework.context.annotation.ScopedProxyMode;
 
 import vn.techmaster.demobean.bean.StreetMap;
 
 @Configuration
 public class MapConfig {
   @Bean
-  @Scope("prototype") //Đừng khởi tạo đối tượng theo kiểu Singleton truyền thống nữa!
+  @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) //Đừng khởi tạo đối tượng theo kiểu Singleton truyền thống nữa!
   //Hãy thử bỏ @Scope("prototype") rồi refresh trình duyệt ở địa chỉ http://localhost:8080/route vài lần
   public StreetMap streetmap() {    
     return new StreetMap();
   }
+
+/*
+  @Bean
+  @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, 
+  proxyMode = ScopedProxyMode.TARGET_CLASS) // Khi không muốn sử dụng @Lazy với @Autowired, 
+  // mà muốn inject 1 prototype bean vào 1 singleton bean, thì có thể sử dụng proxy mode
+  public StreetMap streetmap() {    
+    return new StreetMap();
+  }
+*/
 }
