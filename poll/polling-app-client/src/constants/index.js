df50e8a35a90d208077a957590ab1f43dcbec625
thinhtran279 <thinhtv27988@gmail.com>
2022-01-15 09:29:50 +0700

    Add client

---
+++ poll/polling-app-client/src/constants/index.js
@@ -0,0 +1,18 @@
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+export const ACCESS_TOKEN = 'accessToken';
+
+export const POLL_LIST_SIZE = 30;
+export const MAX_CHOICES = 6;
+export const POLL_QUESTION_MAX_LENGTH = 140;
+export const POLL_CHOICE_MAX_LENGTH = 40;
+
+export const NAME_MIN_LENGTH = 4;
+export const NAME_MAX_LENGTH = 40;
+
+export const USERNAME_MIN_LENGTH = 3;
+export const USERNAME_MAX_LENGTH = 15;
+
+export const EMAIL_MAX_LENGTH = 40;
+
+export const PASSWORD_MIN_LENGTH = 6;
+export const PASSWORD_MAX_LENGTH = 20;
